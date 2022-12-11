587ddb912853e92d10af4e851a2a688e98da5ef7
ahsan.iftikhar <ahsan.iftikhar@paysyslabs.com>
2022-12-12 01:23:16 +0500

    Portfolio

---
+++ client/build/precache-manifest.1fe5665871dda64a27907f8d301de61e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025a192d93e848fc642d8cdad6c8f9e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a666a0eae58956d8b3b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c8b84da1b4b28bb545db",
+    "url": "/static/css/main.49b69339.chunk.css"
+  },
+  {
+    "revision": "6a666a0eae58956d8b3b",
+    "url": "/static/js/2.48af525f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.48af525f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b84da1b4b28bb545db",
+    "url": "/static/js/main.de46fbf7.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a82040ecf06139df2f3e653326b3420c",
+    "url": "/static/media/Bangers-Regular.a82040ec.ttf"
+  },
+  {
+    "revision": "30dfae6cbf1448a606b341cdfb86e5ab",
+    "url": "/static/media/avatar1.30dfae6c.jpg"
+  }
+]);
