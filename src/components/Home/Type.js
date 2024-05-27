2ae32e2646fea815b95f48f25325538c03e68bf9
ahsan.iftikhar <ahsan.iftikhar@paysyslabs.com>
2024-05-27 18:59:23 +0500

    resume updated

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,7 +7,7 @@ function Type() {
       options={{
         strings: [
           "Software Engineer",
-          "Freelancer",
+          "Free-lancer",
           "MERN Stack Developer",
           "Full Stack Engineer"
         ],
--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,8 @@ import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import pdf from "../../Assets/../Assets/AhsanIftikharResume.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
+import { Document, Page } from '@react-pdf/renderer';
+import { pdfjs } from 'pdfjs-dist';
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
@@ -32,14 +33,14 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <div style={{overflow:'hidden'}}>
+          <div style={{ overflow: 'hidden' }}>
             <Document file={pdf} className="d-flex justify-content-center" >
               <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.} />
             </Document>
           </div>
         </Row>
 
-        <Row style={{ justifyContent: "center", position: "relative" , marginTop:'40px' }}>
+        <Row style={{ justifyContent: "center", position: "relative", marginTop: '40px' }}>
           <Button
             variant="primary"
             href={pdf}
